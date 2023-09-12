b3656c3b4d546741acfe67921e551862a0d38f29
ido evergeen <dicksonevegreenido@gmail.com>
2023-09-13 00:41:52 +0100

    add preview

--- next.config.js
+++ next.config.js
@@ -1,28 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
         // pathname: '/account123/**',
       },
 
       {
-        protocol :'https',
-        hostname:'s3-alpha.figma.com',
-        port: '',
-
-      }
+        protocol: "https",
+        hostname: "s3-alpha.figma.com",
+        port: "",
+      },
     ],
   },
   experimental: {
     serverActions: true,
-
-
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
