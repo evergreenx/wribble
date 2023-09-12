8cf1d11eedeb867747050299aa619adfc8102fe4
ido evergeen <dicksonevegreenido@gmail.com>
2023-09-12 10:24:03 +0100

    add dynamcs

--- components/Header/Index.tsx
+++ components/Header/Index.tsx
@@ -7,6 +7,8 @@ import { cookies } from "next/headers";
 
 const links = ["Find Talent", "For Designers", "Learn Design", "Go Pro"];
 
+export const dynamic = "force-dynamic";
+
 async function Index() {
 
   const supabase = createServerComponentClient({ cookies });
