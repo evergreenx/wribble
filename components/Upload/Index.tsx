b1323a02a1a0d83824368f2449d53643b3e703c5
ido evergeen <dicksonevergreenido@gmail.com>
2023-09-18 11:32:32 +0100

    add image preview

--- components/Upload/Index.tsx
+++ components/Upload/Index.tsx
@@ -3,6 +3,7 @@ import { useDropzone } from "react-dropzone";
 import Image from "next/image";
 import UploadIcon from "@/assets/upload.svg";
 import { motion } from "framer-motion";
+import * as Toast from "@radix-ui/react-toast";
 
 interface UploadProps {
   selectedImage: string;
@@ -12,45 +13,62 @@ interface UploadProps {
 const Upload: React.FC<UploadProps> = ({ selectedImage, setSelectedImage }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [sizeError, setSizeError] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const eventDateRef = React.useRef(new Date());
+  const timerRef = React.useRef(0);
 
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    acceptedFiles.forEach((file: File) => {
-      const reader = new FileReader();
+  React.useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      acceptedFiles.forEach((file: File) => {
+        const reader = new FileReader();
 
-      reader.onabort = () => console.log("file reading was aborted");
-      reader.onerror = () => console.log("file reading has failed");
-      reader.onload = () => {
-        const binaryStr = reader.result as ArrayBuffer;
-        const imageUrl = URL.createObjectURL(file);
+        reader.onabort = () => console.log("file reading was aborted");
+        reader.onerror = () => console.log("file reading has failed");
+        reader.onload = () => {
+          const binaryStr = reader.result as ArrayBuffer;
+          const imageUrl = URL.createObjectURL(file);
 
-        // Check image dimensions
-        const image = document.createElement("img"); 
-        image.src = imageUrl;
-        image.onload = () => {
-          if (image.width < 400 || image.height < 300) {
-            setErrorMessage(
-              "Go big or go home. Your file's dimensions should be at least 400x300."
-            );
-          } else {
-            setSelectedImage(imageUrl);
-            setErrorMessage(""); // Clear any previous error message
-          }
+          // Check image dimensions
+          const image = document.createElement("img");
+          image.src = imageUrl;
+          image.onload = () => {
+            if (image.width < 400 || image.height < 300) {
+              setOpen(true);
+              setErrorMessage(
+                "Go big or go home. Your file's dimensions should be at least 400x300."
+              );
+            } else {
+              setSelectedImage(imageUrl);
+              setErrorMessage(""); // Clear any previous error message
+            }
+          };
         };
-      };
-      reader.readAsArrayBuffer(file);
-    });
-  }, [setSelectedImage]);
+        reader.readAsArrayBuffer(file);
+      });
+    },
+    [setSelectedImage]
+  );
 
   const maxSizeBytes = 5 * 1024 * 1024; // 5MB max size
   const minSizeBytes = 1024; // 1KB min size
 
   const validateFile = (file: File) => {
     if (file.size > maxSizeBytes) {
-      alert("File is too large. Maximum size is 5MB.");
+      setOpen(true);
+
+      setSizeError("File is too large. Maximum size is 5MB.");
+
       return false;
     }
     if (file.size < minSizeBytes) {
-      alert("File is too small. Minimum size is 1KB.");
+      setOpen(true);
+
+      setSizeError("File is too small. Minimum size is 1KB.");
+
       return false;
     }
     return true;
@@ -64,9 +82,9 @@ const Upload: React.FC<UploadProps> = ({ selectedImage, setSelectedImage }) => {
       }
     },
     accept: {
-        'image/jpeg': [],
-        'image/png': []
-      },
+      "image/jpeg": [],
+      "image/png": [],
+    },
     maxSize: maxSizeBytes,
   });
 
@@ -81,19 +99,16 @@ const Upload: React.FC<UploadProps> = ({ selectedImage, setSelectedImage }) => {
           transition={{ duration: 0.5 }}
           className="selected-image "
         >
-          <Image src={selectedImage} alt="Selected" className="rounded-2xl w-full h-auto" 
-          
-
-         
-
-          width={600}
-          height={600}
-          quality={100}
-          priority={true}
-          placeholder ='blur'
-          blurDataURL= {selectedImage}
-
-    
+          <Image
+            src={selectedImage}
+            alt="Selected"
+            className="rounded-2xl w-full h-auto"
+            width={600}
+            height={600}
+            quality={100}
+            priority={true}
+            placeholder="blur"
+            blurDataURL={selectedImage}
           />
         </motion.div>
       ) : (
@@ -117,10 +132,7 @@ const Upload: React.FC<UploadProps> = ({ selectedImage, setSelectedImage }) => {
             <input {...getInputProps()} />
 
             <div className="justify-center items-center mx-auto flex">
-              <Image src={UploadIcon} alt="upload"
-
-              
-              />
+              <Image src={UploadIcon} alt="upload" />
             </div>
             <p className="text-sm my-3">
               {isDragActive
@@ -128,10 +140,31 @@ const Upload: React.FC<UploadProps> = ({ selectedImage, setSelectedImage }) => {
                 : "Drag 'n' drop an image file, or "}
               <span className="underline"> browse</span>
             </p>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           </motion.div>
         </div>
       )}
+
+      <Toast.Provider duration={2000} swipeDirection="up">
+        <Toast.Root
+          className="text-[#fff] bg-black rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_'title_action'_'description_action'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut"
+          open={open}
+          onOpenChange={setOpen}
+        >
+          <Toast.Title className="[grid-area:_title] mb-[5px] font-medium text-slate12 text-[14px]">
+            {errorMessage || sizeError}
+          </Toast.Title>
+          <Toast.Close aria-label="Close">
+            <span aria-hidden>×</span>
+          </Toast.Close>
+          <Toast.Description asChild></Toast.Description>
+          <Toast.Action
+            className="[grid-area:_action]"
+            asChild
+            altText="Goto schedule to undo"
+          ></Toast.Action>
+        </Toast.Root>
+        <Toast.Viewport className="[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none" />
+      </Toast.Provider>
     </div>
   );
 };
