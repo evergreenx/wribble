88242299369d2e799147eb58af9a4d4daed30a20
ido evergeen <dicksonevergreenido@gmail.com>
2023-10-09 02:08:15 +0100

    fix webkit error

--- components/Switch/Index.tsx
+++ components/Switch/Index.tsx
@@ -10,7 +10,7 @@ const SwitchFeedBack = () => (
       <Switch.Root
         className="w-[38px] h-[22px] bg-[#e7e7e9] border-none rounded-full relative  shadow-black focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
         id="feedback-mode"
-        style={{ '-webkit-tap-highlight-color': 'rgba(0, 0, 0, 0)' }}
+        // style={{ '-webkit-tap-highlight-color': 'rgba(0, 0, 0, 0)' }}
       >
         <Switch.Thumb className="block w-[14px] h-[14px] bg-[#9e9ea7] rounded-full shadow-[0_2px_2px] shadow-blackA7 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
       </Switch.Root>
